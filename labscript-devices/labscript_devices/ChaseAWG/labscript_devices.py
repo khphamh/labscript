69ffc365dc6f7dfcfbc7d8b92cb6c117ac872f48
khphamh <100740069+khphamh@users.noreply.github.com>
2023-03-07 09:09:30 -0500

    Add files via upload

--- labscript-devices/labscript_devices/ChaseAWG/labscript_devices.py
+++ labscript-devices/labscript_devices/ChaseAWG/labscript_devices.py
@@ -1,6 +1,32 @@
 from labscript_devices import labscript_device, BLACS_tab, BLACS_worker
-from labscript import IntermediateDevice, DigitalOut, AnalogOut, config
+from labscript import IntermediateDevice, DigitalOut, AnalogOut, config, DDSQuantity, DDS, LabscriptError
 import numpy as np
+import h5py
+
+
+class ChaseAWGDDS(DDSQuantity):
+    description = 'ChaseAWGDDS'
+    def __init__(self, *args, **kwargs):
+        if 'call_parents_add_device' in kwargs:
+            call_parents_add_device = kwargs['call_parents_add_device']
+        else:
+            call_parents_add_device = True
+
+        kwargs['call_parents_add_device'] = False
+        DDSQuantity.__init__(self, *args, **kwargs)
+
+        if call_parents_add_device:
+            self.parent_device.add_device(self)
+
+        self.channel_list = [ ]
+        self.NumPoints_list = [ ]
+        self.NumLoops_list = [ ]
+        self.Waveform_list = [ ]
+    def add_pulse(self,channel, NumPoints, NumLoops, Waveform):
+        self.channel_list.append(channel)
+        self.NumPoints_list.append(NumPoints)
+        self.NumLoops_list.append(NumLoops)
+        self.Waveform_list.append(bytes(Waveform,'utf-8'))
 
 class ChaseAWG(IntermediateDevice):
 
@@ -8,38 +34,43 @@ class ChaseAWG(IntermediateDevice):
     clock_limit = 1e6
 
     # If this is updated, then you need to update generate_code to support whatever types you add
-    allowed_children = [DigitalOut, AnalogOut]
+    allowed_children = [DigitalOut, AnalogOut, DDS, ChaseAWGDDS]
 
     def __init__(self, name, parent_device, BLACS_connection='dummy_connection', **kwargs):
         self.BLACS_connection = BLACS_connection
         IntermediateDevice.__init__(self, name, parent_device, **kwargs)
 
+
+    def _make_ChaseAWG_settings_table(self, inputs):
+        """Collect analog input instructions and create the acquisition table"""
+        if not inputs:
+            return None
+
+        for connection, input in inputs.items():
+            DDS_channels = input.__dict__['channel_list']
+            DDS_NumPoints = input.__dict__['NumPoints_list']
+            DDS_NumLoops = input.__dict__['NumLoops_list']
+            DDS_Waveform = input.__dict__['Waveform_list']
+
+            #raise LabscriptError(pulse_list_2)
+        DDS_table = [DDS_channels, DDS_NumPoints, DDS_NumLoops, DDS_Waveform]
+            #raise LabscriptError(DDS_table)
+        #raise LabscriptError(DDS_table)
+        return DDS_table
+
     def generate_code(self, hdf5_file):
         IntermediateDevice.generate_code(self, hdf5_file)
-        group = self.init_device_group(hdf5_file)
-
-        clockline = self.parent_device
-        pseudoclock = clockline.parent_device
-        times = pseudoclock.times[clockline]
-
-        # out_table = np.empty((len(times),len(self.child_devices)), dtype=np.float32)
-        # determine dtypes
-        dtypes = []
+        DDS_set = {}
         for device in self.child_devices:
-            if isinstance(device, DigitalOut):
-                device_dtype = np.int8
-            elif isinstance(device, AnalogOut):
-                device_dtype = np.float64
-            dtypes.append((device.name, device_dtype))
+            if isinstance(device, (DDS, ChaseAWGDDS)):
+                DDS_set[device.connection] = device 
+        DDStable = self._make_ChaseAWG_settings_table(DDS_set)
+        #raise LabscriptError(DDStable)
+        dt = h5py.string_dtype(encoding='utf-8') 
+        grp = self.init_device_group(hdf5_file)
 
-        # create dataset
-        out_table = np.zeros(len(times), dtype=dtypes)
-        for device in self.child_devices:
-            out_table[device.name][:] = device.raw_output
-
-        group.create_dataset('OUTPUTS', compression=config.compression, data=out_table)
-
-
+        if DDStable is not None:
+            grp.create_dataset('DDS', data=DDStable, compression=config.compression)
 
 
 ''' from labscript_devices import labscript_device, BLACS_tab, BLACS_worker
